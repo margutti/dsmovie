e951ec01550f67c8371d1869a1e922400ab3cd11
margutti <margutti@softmidia.com.br>
2022-01-15 21:45:54 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,22 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 import './styles.css';
 
 function Listing() {
 
-    // wrong 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
         });
+    }, []);
 
     return (
         <>
